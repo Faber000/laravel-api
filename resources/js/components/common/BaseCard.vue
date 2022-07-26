f0e8e0f06223f9a1b7abcb34bbe6aa62ede816f3
Fabio Moro <fabio.moro2000@gmail.com>
2022-07-26 17:52:39 +0200

    aggiunta home page

---
+++ resources/js/components/common/BaseCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="card">
+    <h2>{{title}}</h2>
+    <div>{{content}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'BaseCard',
+    props: {
+        title: {
+            type: String
+        },
+        slug: {
+            type: String,
+        },
+        content: {
+            type: String
+        },  
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.card {
+    background-color: white;
+    border-radius: 10%;
+    padding: 10px;
+}
+</style>
